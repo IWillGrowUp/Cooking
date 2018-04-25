b854970ccca484350905c8c3f7eec3b2730b297a
Mike <mikhail.romanishin@gmail.com>
2018-04-25 16:32:27 +0000

    some js added

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+function showRecipe (someDish, url) {
+    document.getElementById(someDish).addEventListener('click', recipe);
+
+function recipe() {
+    document.getElementById('main_sect').style.display = 'none';
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('insert_here').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open('GET', url + '?r=' + Math.random(), true);
+    xhr.send();
+    }
+}
+
+showRecipe('chocolateCakeWithKuraga', 'chocolate_cake_with_kuraga.html');
