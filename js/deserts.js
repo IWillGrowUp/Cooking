19ba81251d84d994e7b42b0d762aae8588bd3052
Mike <mikhail.romanishin@gmail.com>
2018-05-03 16:07:27 +0000

    fontawesome + favicon + navbar collapse

--- js/deserts.js
+++ js/deserts.js
@@ -13,25 +13,3 @@ $(document).on( 'click', someDesert, function desertRecipe() {
 }
 
 showDesert('#appleJam', 'recipes/apple_jam.html');
-
-
-
-/*
-function showDesert (someDesert, desertUrl) {
-    document.getElementById(someDesert).addEventListener('click', desertRecipe);
-
-function desertRecipe() {
-    document.getElementById('main_sect').style.display = 'none';
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById('insert_here').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', desertUrl + '?r=' + Math.random(), true);
-    xhr.send();
-    }
-}
-
-showDesert('appleJam', '../recipes/apple_jam.html');
-*/
